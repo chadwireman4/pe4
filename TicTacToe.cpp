5abb16aec966c440cbd6c627d0c4288922a08ae0
chadwireman4 <chwi1755@colorado.edu>
2020-02-11 19:08:26 -0700

    added DisplayBoard function

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -12,7 +12,28 @@ void CreateBoard(TileFill board[3][3]) {
 	}
 }
 
+void DisplayBoard(TileFill board[3][3]) {
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			switch(board[i][j]) {
+				case TileFill::Empty:
+					cout << "| ";
+					break;
+				case TileFill::X:
+					cout << "|X";
+					break;
+				case TileFill::O:
+					cout << "|O";
+					break;
+			}
+		}
+
+		cout << "|" << endl;
+	}
+}
+
 int main() {
 	TileFill board[3][3];
 	CreateBoard(board);
+	DisplayBoard(board);
 }
